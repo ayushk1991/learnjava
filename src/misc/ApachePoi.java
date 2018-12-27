28059dfd19416b04c7638149213b3500f90f75fb
Ayush Kumar <staravtar@gmail.com>
2018-12-27 21:24:42 +0530

    new update

--- src/misc/ApachePoi.java
+++ src/misc/ApachePoi.java
@@ -1,9 +1,28 @@
 package misc;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ApachePoi {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		String filelocation=System.getProperty("user.dir")+"\\data.xlsx";
+		System.out.println(filelocation);
+		
+		File xlfile=new File(filelocation);
+		FileInputStream filein=new FileInputStream(xlfile);
+		
+		Workbook wb= new XSSFWorkbook(filein);
+		
+		Sheet xlsheet= wb.getSheet("Sheet1");
 
 	}
 
