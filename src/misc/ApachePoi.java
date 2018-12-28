2a9398ea2c7db070fbc08734edfbe4c1c97ce3bc
ayushk1991 <37550040+ayushk1991@users.noreply.github.com>
2018-12-29 02:26:07 +0530

    update

--- src/misc/ApachePoi.java
+++ src/misc/ApachePoi.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -42,7 +43,7 @@ public class ApachePoi {
 		filein.close();		
 	}
 	
-	public static void fileout() throws IOException, InterruptedException
+	public static void fileout() throws IOException, InterruptedException, InvalidFormatException
 	{
 		
 		String filelocation=System.getProperty("user.dir")+"\\data.xlsx";
